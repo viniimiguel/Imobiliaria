f8a950f4da1d85ed42a3c7cd69b3902336381c58
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2023-10-23 23:56:21 -0300

    askdop

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ class Imovel():
         self.entra()
         sleep(5)
         self.raspagem()
+        sleep(5)
+        self.cria_planilhas()
         sleep(12831830)
 
 
@@ -28,43 +30,82 @@ class Imovel():
         self.driver.maximize_window()
 
     def raspagem(self):
+        global armazena_logradouro, armazena_rua, armazena_descricao, armazena_info, armazena_valor
+
+        armazena_logradouro = []
+        armazena_rua = []
+        armazena_descricao = []
+        armazena_info = []
+        armazena_valor = []
+        
         contador = 1
+
         
         while True:
             try:
                 xpats = {
                     'XP': {
                         'logradouro': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[{contador}]/div/a/div/div/div[2]/div[1]/section/div/h2',
-                        'rua': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/div[1]/section/p',
-                        'descricao': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/div[2]/p',
-                        'metragem': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/section/p[1]',
-                        'comodos': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/section/p[2]',
-                        'banheiros': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/section/p[3]',
-                        'vagas': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/section/p[4]',
-                        'valor': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/div[3]/div[1]/p',
+                        'rua': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[{contador}]/div/a/div/div/div[2]/div[1]/section/p',
+                        'descricao': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[{contador}]/div/a/div/div/div[2]/div[2]/p',
+                        'info': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[{contador}]/div/a/div/div/div[2]/section',
+                        'valor': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[{contador}]/div/a/div/div/div[2]/div[3]/div[1]/p',
+                        'button_next': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[2]/div/section/div/div[2]/div[2]/nav/button[2]',
                     }
                 }
 
                 logradouro = self.driver.find_element(By.XPATH, xpats['XP']['logradouro'])
+                armazena_logradouro.append(logradouro.text)
+
                 rua = self.driver.find_element(By.XPATH, xpats['XP']['rua'])
+                armazena_rua.append(rua.text)
+
                 descricao = self.driver.find_element(By.XPATH, xpats['XP']['descricao'])
-                metragem = self.driver.find_element(By.XPATH, xpats['XP']['metragem'])
-                comodos = self.driver.find_element(By.XPATH, xpats['XP']['comodos'])
-                banheiros = self.driver.find_element(By.XPATH, xpats['XP']['banheiros'])
-                vagas = self.driver.find_element(By.XPATH, xpats['XP']['vagas'])
+                armazena_descricao.append(descricao.text)
+
+                info = self.driver.find_element(By.XPATH, xpats['XP']['info'])
+                armazena_info.append(info.text)
+
                 valor = self.driver.find_element(By.XPATH, xpats['XP']['valor'])
+                armazena_valor.append(valor.text)
+
+                self.driver.execute_script('arguments[0].scrollIntoView();', logradouro)
+
+           
 
                 print(logradouro.text)
                 print(rua.text)
                 print(descricao.text)
-                print(metragem.text)
-                print(comodos.text)
-                print(banheiros.text)
-                print(vagas.text)
+                print(info.text)
                 print(valor.text)
+                print('###'*40)
+                sleep(0.5)
+
+                contador += 1
+
 
             except NoSuchElementException:
-                print('nao tem mais elementos nesta pagina')
+                break
+    
+    def cria_planilhas(self):
+        planilha = openpyxl.Workbook()
+        imovel = planilha.active
+        imovel.title = 'Imoveis'
+        imovel['A1'] = 'Logradouro'
+        imovel['B1'] = 'Rua'
+        imovel['C1'] = 'Descrição'
+        imovel['D1'] = 'Info'
+        imovel['E1'] = 'Valor'
+
+        for index, (logradouro, rua, descricao, info, valor) in enumerate(zip(armazena_logradouro, armazena_rua, armazena_descricao, armazena_info, armazena_valor), start=2):
+            imovel.cell(column=1, row=index, value=logradouro)
+            imovel.cell(column=2, row=index, value=rua)
+            imovel.cell(column=3, row=index, value=descricao)
+            imovel.cell(column=4, row=index, value=info)
+            imovel.cell(column=5, row=index, value=valor)
+
+        planilha.save('planilha_de_precos_atacadao.xlsx')
+        print('planilha salva com sucesso!')
 
 
 imovel = Imovel()
