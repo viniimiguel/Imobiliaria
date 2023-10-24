b5fe00d67ed8481f758067fddeb45cba4eca0c91
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2023-10-23 22:41:35 -0300

    second commit

--- app.py
+++ app.py
@@ -14,12 +14,8 @@ from selenium.webdriver.common.keys import Keys
 class Imovel():
     def __init__(self):
         self.driver = webdriver.Chrome()
-        self.site_link = 'https://www.vivareal.com.br/venda/'
-        self.site_map = {
-            'XP': {
-                'pesquisa': '/html/body/main/div[2]/div[1]/nav/div/div/form/fieldset[1]/div[1]/div/div/input',
-            }
-        }
+        self.site_link = 'https://www.zapimoveis.com.br/venda/imoveis/pe+recife/?__ab=exp-aa-test:B,new-discover-zap:alert,vas-officialstore-social:enabled,deduplication:control&transacao=venda&onde=,Pernambuco,Recife,,,,,city,BR>Pernambuco>NULL>Recife,-8.05774,-34.882963,&pagina=1'
+        
     def main(self):
         self.entra()
         sleep(5)
@@ -30,47 +26,45 @@ class Imovel():
     def entra(self):
         self.driver.get(self.site_link)
         self.driver.maximize_window()
-        pesq = input('digite oque voce quer procurar: ')
-        rua = self.driver.find_element(By.XPATH, self.site_map['XP']['pesquisa'])
-        rua.send_keys(pesq)
-        sleep(3)
-        rua.send_keys(Keys.ENTER)
 
     def raspagem(self):
         contador = 1
+        
         while True:
-            definicoes = {
-                'XP': {
-                    'rua': f'/html/body/main/div[2]/div[1]/section/div[2]/div[1]/div[{contador}]/div/article/a/div/h2/span[2]/span[1]',
-                    'descricao': f'/html/body/main/div[2]/div[1]/section/div[2]/div[1]/div[{contador}]/div/article/a/div/h2/span[1]',
-                    'metragem': f'/html/body/main/div[2]/div[1]/section/div[2]/div[1]/div[{contador}]/div/article/a/div/ul[1]',
-                    'valorizacao': f'/html/body/main/div[2]/div[1]/section/div[2]/div[1]/div[{contador}]/div/article/a/div/ul[2]',
-                    'valor': f'/html/body/main/div[2]/div[1]/section/div[2]/div[1]/div[{contador}]/div/article/a/div/section/div'
-                }
-            }
             try:
-                localizacao = self.driver.find_element(By.XPATH, definicoes['XP']['rua'])
-                descricao = self.driver.find_element(By.XPATH, definicoes['XP']['descricao'])
-                metragem = self.driver.find_element(By.XPATH, definicoes['XP']['metragem'])
-                valorizacao = self.driver.find_element(By.XPATH, definicoes['XP']['valorizacao'])
-                valor = self.driver.find_element(By.XPATH, definicoes['XP']['valor'])
+                xpats = {
+                    'XP': {
+                        'logradouro': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[{contador}]/div/a/div/div/div[2]/div[1]/section/div/h2',
+                        'rua': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/div[1]/section/p',
+                        'descricao': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/div[2]/p',
+                        'metragem': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/section/p[1]',
+                        'comodos': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/section/p[2]',
+                        'banheiros': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/section/p[3]',
+                        'vagas': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/section/p[4]',
+                        'valor': f'/html/body/div[1]/main/section/div/form/div[2]/div[2]/div[1]/div[2]/div[1]/div/a/div/div/div[2]/div[3]/div[1]/p',
+                    }
+                }
 
-                print(localizacao.text)
+                logradouro = self.driver.find_element(By.XPATH, xpats['XP']['logradouro'])
+                rua = self.driver.find_element(By.XPATH, xpats['XP']['rua'])
+                descricao = self.driver.find_element(By.XPATH, xpats['XP']['descricao'])
+                metragem = self.driver.find_element(By.XPATH, xpats['XP']['metragem'])
+                comodos = self.driver.find_element(By.XPATH, xpats['XP']['comodos'])
+                banheiros = self.driver.find_element(By.XPATH, xpats['XP']['banheiros'])
+                vagas = self.driver.find_element(By.XPATH, xpats['XP']['vagas'])
+                valor = self.driver.find_element(By.XPATH, xpats['XP']['valor'])
+
+                print(logradouro.text)
+                print(rua.text)
                 print(descricao.text)
                 print(metragem.text)
-                print(valorizacao.text)
+                print(comodos.text)
+                print(banheiros.text)
+                print(vagas.text)
                 print(valor.text)
-                print('###'*25)
-                contador += 1
-                if contador == 10:
-                    break
+
             except NoSuchElementException:
-                print('elemento nao encontrado')
-
-
-        
-
-
+                print('nao tem mais elementos nesta pagina')
 
 
 imovel = Imovel()
